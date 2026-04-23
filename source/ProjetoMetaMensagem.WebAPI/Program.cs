c72c30230c2b2c0ea904a1605955313bc7b773d2
Administrator <Administrator@DESKTOP-QR4BNH8>
2026-04-23 18:26:42 -0300

    Adicionado projeto do Front e ajustado

--- source/ProjetoMetaMensagem.WebAPI/Program.cs
+++ source/ProjetoMetaMensagem.WebAPI/Program.cs
@@ -45,7 +45,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowReactApp",
         policy =>
         {
-            policy.WithOrigins("http://localhost:5173", "http://localhost:3000") // Portas comuns do React/Vite
+            policy.WithOrigins("http://localhost:5173", "http://localhost:3000", "http://localhost:4200") // Portas comuns do React/Vite
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
