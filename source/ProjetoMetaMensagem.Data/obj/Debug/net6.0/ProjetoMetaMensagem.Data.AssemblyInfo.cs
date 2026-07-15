4914daf2ab99b609805a49d3a2a9e07fd097644f
Administrator <Administrator@DESKTOP-QR4BNH8>
2026-07-14 22:17:18 -0300

    Voltado pro estado base

--- source/ProjetoMetaMensagem.Data/obj/Debug/net6.0/ProjetoMetaMensagem.Data.AssemblyInfo.cs
+++ source/ProjetoMetaMensagem.Data/obj/Debug/net6.0/ProjetoMetaMensagem.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoMetaMensagem.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+771e600059cc01ec05fd40466a196e7a03f60a04")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoMetaMensagem.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoMetaMensagem.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
